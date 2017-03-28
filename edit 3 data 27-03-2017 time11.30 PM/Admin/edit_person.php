9ebd85df97a16ccac23f478d9bf396b6782eb2cf
thirdppp <third1025@hotmail.com>
2017-03-29 02:16:15 +0700

    แก้บัคตรง for="header"
    ใส่คำผิดเวลาแก้ไขเลยบัค

--- edit 3 data 27-03-2017 time11.30 PM/Admin/edit_person.php
+++ edit 3 data 27-03-2017 time11.30 PM/Admin/edit_person.php
@@ -1,7 +1,7 @@
 <html>
 
     <head>
-        <title>Edit_news</title>
+        <title>Edit_person</title>
        <?php
 
                 include('StyleHandle.css');
@@ -14,9 +14,9 @@
         <?php
          include('../Logig/ManagePerson.php');
         $obj = new ManagePerson;
-        if(isset($_POST['submitPerson'])&&isset($_GET['id'])){
+        if(isset($_POST['submitEditPerson'])&&isset($_GET['id'])){
             if(isset($_POST)){
-            echo $obj->Edit_person($_POST['position'],$_POST['name'],$_GET['id'],$_POST['lastname'],$_POST['image']);
+            echo $obj->Edit_person($_POST['position'],$_POST['name'],$_POST['lastname'],$_GET['id'],$_POST['image']);
             }
         }else{
             echo "Noooo";
@@ -24,31 +24,31 @@
         $row = $obj->Select_person($_GET['id']);
         ?>
 
-        <h2>Edit_News</h2>
+        <h2>Edit_person</h2>
 
         <form class="form" method="post" action="#">
             <p class="name">
 
                 <input type="text" name="position" id="position"
-                 placeholder="header news" value="<?php  echo $row['position'];?>" />
-                <label for="header">ตำแหน่ง</label>
+                 placeholder="position" value="<?php  echo $row['position'];?>" />
+                <label for="position">ตำแหน่ง</label>
             </p>
             <p class="name">
 
                 <input type="text" name="name" id="name"
-                 placeholder="header news" value="<?php  echo $row['name'];?>" />
-                <label for="header">ชื่อ</label>
+                 placeholder="name" value="<?php  echo $row['name'];?>" />
+                <label for="name">ชื่อ</label>
             </p>
             <p class="name">
 
                 <input type="text" name="lastname" id="lastname"
-                 placeholder="header news" value="<?php  echo $row['lastname'];?>" />
-                <label for="header">นามสกุล</label>
+                 placeholder="lastname" value="<?php  echo $row['lastname'];?>" />
+                <label for="lastname">นามสกุล</label>
             </p>
 
            Select Main Image:
-                <input type="file" name="image" value="<?php "../assets/css/img".$row['nameimg']?>"/><br><br>
-                <input type="submit" name="submitPerson" value="Edit Person"/>
+           <input type="file" name="image"/><br><br>
+           <input type="submit" name="submitEditPerson" value="Edit Person"/>
         </form>
 
     </body>
